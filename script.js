6afb18f6367fd18483f239f87350386e90436d49
Jetsada105 <jessadatrad@gmail.com>
2025-11-13 10:39:07 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function scrollToSection(id) {
-  document.getElementById(id).scrollIntoView({ behavior: "smooth" });
-}
-
-document.querySelector("form").addEventListener("submit", (e) => {
-  e.preventDefault();
-  alert("ขอบคุณสำหรับข้อความของคุณ! We’ll get back to you soon.");
-});
-
